d4e53078aa1192d51f65e38909bd03a256b694c7
943161238@qqcom <943161238@qq.com>
2019-10-14 14:10:35 +0800

    03-首页导航布局完成

--- src/components/HKLayouts/index.js
+++ src/components/HKLayouts/index.js
@@ -37,10 +37,10 @@ class HKLayouts extends React.Component {
             }
             selected={this.props.location.pathname === "/"}
             badge={1}
-            onPress={() => {
+            onPress={() => {this.props.history.push('/')}}
 
-              this.props.history.push('/')
-            }}
+              
+            
             data-seed="logId"
           >
             {this.props.location.pathname === "/" && this.props.children}
@@ -64,11 +64,7 @@ class HKLayouts extends React.Component {
             key="找房"
             badge={'new'}
             selected={this.props.location.pathname === "/List"}
-            onPress={() => {
-             
-              this.props.history.push('/List')
-
-            }}
+            onPress={() => {this.props.history.push('/List')}}
             data-seed="logId1"
           >
             {this.props.location.pathname === "/List" && this.props.children}
@@ -92,11 +88,7 @@ class HKLayouts extends React.Component {
             key="咨询"
             dot
             selected={this.props.location.pathname === "/News"}
-            onPress={() => {
-             
-              this.props.history.push('/News')
-
-            }}
+            onPress={() => {this.props.history.push('/News')}}
           >
              {this.props.location.pathname === "/News" && this.props.children}
           </TabBar.Item>
@@ -106,10 +98,7 @@ class HKLayouts extends React.Component {
             title="我的"
             key="我的"
             selected={this.props.location.pathname === "/My"}
-            onPress={() => {
-              this.props.history.push('/My')
-
-            }}  
+            onPress={() => {this.props.history.push('/My')}}  
           >
              {this.props.location.pathname === "/My" && this.props.children}
           </TabBar.Item>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,14 +7,28 @@ import axios from '../../utils/request'
 import { baseUrl } from '../../utils/url'
 
 // 把本地的图片当成js文件一样来引入即可
-// import imgdemo from '../../assets/images'
+import nav1 from '../../assets/images/nav-1.png'
+import nav2 from '../../assets/images/nav-2.png'
+import nav3 from '../../assets/images/nav-3.png'
+import nav4 from '../../assets/images/nav-4.png'
+
+// 引入局部样式
+import HKindex from './HKindx.module.css'
 export default class Home extends Component {
     state = {
       // 轮播图
         swiperList: [],
         // 解决轮播图高度bug
         imgHeight: 176,
-    }
+
+        // 首页导航
+        indexNav: [
+					{id:1,title: "整组",imgSrc: nav1},
+					{id:2,title: "合租",imgSrc: nav2},
+					{id:3,title: "地图找房",imgSrc: nav3},
+					{id:4,title: "去出租",imgSrc: nav4},
+				],				
+	}
 
 // 在组件创建完后发送请求
     componentDidMount(){
@@ -32,30 +46,46 @@ export default class Home extends Component {
         <Fragment>
           {/* 轮播图开始 */}
           {/* 该组件没有数据时不要渲染，要等到轮播有数据后才渲染 */}
-         {this.state.swiperList.length && <Carousel
-              autoplay
-              infinite
-            >
-              {this.state.swiperList.map(val => (
-                <a
-                  key={val.id}
-                  href="http://www.alipay.com"
-                  style={{ display: 'inline-block', width: '100%' }}
-                >
-                  <img
-                    src={baseUrl + val.imgSrc}
-                    alt=""
-                    style={{ width: '100%', verticalAlign: 'top', height: this.state.imgHeight}}
-                    onLoad={() => {
-                      // fire window resize event to change height
-                      window.dispatchEvent(new Event('resize'));
-                      this.setState({ imgHeight: 'auto' });
-                    }}
-                  />
-                </a>
-              ))}
-            </Carousel>} 
-            {/* 轮播图结束  */}
+          <div>
+            {this.state.swiperList.length && <Carousel
+                autoplay
+                infinite
+              >
+                {this.state.swiperList.map(val => (
+                  <a
+                    key={val.id}
+                    href="http://www.alipay.com"
+                    style={{ display: 'inline-block', width: '100%' }}
+                  >
+                    <img
+                      src={baseUrl + val.imgSrc}
+                      alt=""
+                      style={{ width: '100%', verticalAlign: 'top', height: this.state.imgHeight}}
+                      onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'));
+                        this.setState({ imgHeight: 'auto' });
+                      }}
+                    />
+                  </a>
+                ))}
+              </Carousel>} 
+          </div>
+					 {/* 轮播图结束  */}
+
+           {/* 首页导航结构开始*/}
+					 {/* <div className='hk_nav'> */}
+					 <div className={HKindex.hk_nav}>
+
+						 {this.state.indexNav.map(v=><div className={HKindex.nav_item} key={v.id}>
+							 <div className={HKindex.nav_img_waper}>
+							 		<img src={v.imgSrc}/>
+							 </div>
+							 <span>{v.title}</span>
+						 </div>)}
+					 </div>
+           {/* 首页导航结构结束 */}
+
         </Fragment>       
         );
       }
