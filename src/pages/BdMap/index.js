3b35cbb82d8973c2fc4035cfff7e409455891546
943161238@qqcom <943161238@qq.com>
2019-10-17 20:32:54 +0800

    05-添加全局loading事件 + 清除旧覆盖物

--- src/pages/BdMap/index.js
+++ src/pages/BdMap/index.js
@@ -4,7 +4,7 @@ import { BMap } from '../../utils/map'
 import indexCss from './index.module.scss'
 import store from '../../store'
 import axios from '../../utils/request'
-import { nonsense } from 'antd-mobile/lib/picker';
+
 
 /**
  * 1 根据定位写好
@@ -19,7 +19,8 @@ import { nonsense } from 'antd-mobile/lib/picker';
 
 
 export default class index extends Component {
-
+    // 定义一个全局的地图对象
+    Map = null;
     // 获取当前城市
     constructor(){
         super()
@@ -29,7 +30,7 @@ export default class index extends Component {
         if(!cityName) {
             store.subscribe(()=>{
                cityName = store.getState().mapReducer.cityName
-               this.renderCity(cityName)   
+               this.renderCityMap(cityName)
            })
         }
     }
@@ -38,42 +39,67 @@ export default class index extends Component {
     componentDidMount(){
         let cityName = store.getState().mapReducer.cityName
         if(cityName){
-            this.renderCity(cityName) 
+            this.renderCityMap(cityName) 
         }
     }
 
 
     // 渲染地图
-    renderCity=async (cityName)=>{
+    renderCityMap=async (cityName)=>{
         // console.log(cityName)
         var map = new BMap.Map("allmap");    // 创建Map实例
+        // 全局Map赋值
+        this.Map = map
 	    map.centerAndZoom(cityName,11);  // 初始化地图,设置中心点坐标和地图级别
         //    添加比例尺
         map.addControl(new BMap.ScaleControl());
         // 添加平移缩放控件
         map.addControl(new BMap.NavigationControl());
-        // 1.获取城市ID
-        let id =(await axios.get("/area/info?name="+cityName)).data.body.value 
-        // 2.获取房源信息
-        let list = (await axios.get("/area/map?id="+id)).data.body
-        //    3.获取经纬度
-        list.forEach((v,i) => {
-            const point =  new BMap.Point(v.coord.longitude,v.coord.latitude);
-        const htmlStr = `<div class=${indexCss.label_item}><span>${v.label }</span><span>${ v.count}套</span></div>`
-            const label = this.getLable(point,htmlStr);
-            // 4.把文本描绘到地图上 
-            map.addOverlay(label);
-
-        });
-       
-
-
         
+        // 调用渲染城市label的方法
+        this.renderCityLabel({label: cityName,value: null})
+    };
+
+
+    // 2.抽离公共部分代码，渲染城市label
+    renderCityLabel=async (cityObj)=>{
+       // 判读是否有ID
+       let id = cityObj.value;
+       // 如果没有ID，就发送接口拿ID
+       if(!id) {
+           // 1.获取城市ID
+           id =(await axios.get("/area/info?name="+cityObj.label)).data.body.value 
+
+       }   
+       // 2.获取房源信息
+       let list = (await axios.get("/area/map?id="+id)).data.body
+       // 3.获取经纬度
+       list.forEach((v,i) => {
+           const point =  new BMap.Point(v.coord.longitude,v.coord.latitude);
+           const htmlStr = `<div class=${indexCss.label_item}><span>${v.label }</span><span>${ v.count}套</span></div>`
+           const label = this.getLable(point,htmlStr);
+           // 1.给每个label绑定点击事件
+           label.addEventListener("click",()=>{
+               // console.log(v)  {label: "番禺", value: "AREA|e8e35a38-9f6e-9df8"}
+              this.renderCityLabel(v);
+             setTimeout(()=>{
+                this.Map.clearOverlays()
+             },0)
+           })
+
+           // 4.把文本描绘到地图上 
+           this.Map.addOverlay(label);
+           
+
+       });
+
     }
+    
+
+
 
     // 渲染地图文字标签
     getLable=(point,content)=>{
-
         var opts = {
             position : point,    // 指定文本标注所在的地理位置
             offset   : new BMap.Size(30, -30)    //设置文本偏移量
@@ -83,8 +109,7 @@ export default class index extends Component {
                  border: 'none',
                  backgroundColor: 'transparent'
            });
-        // map.addOverlay(label);  
-        return  label
+        return label
     }
 
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,29 @@
 import axios from 'axios'
+import { Toast} from 'antd-mobile';
+
 // 定义公共的url
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    // 添加loadding事件
+    Toast.loading('加载中...', 0)
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    //   关闭loadding事件
+    Toast.hide()
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
+
 export default axios
